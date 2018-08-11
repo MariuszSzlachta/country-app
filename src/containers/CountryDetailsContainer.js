c1c07fdaf78fdebf1ebbe7c0564d8cf6e1b40993
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-11 14:22:02 +0200

    Add CountryDetailsContainer and CountryDetails

---
+++ src/containers/CountryDetailsContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getCountry } from '../actions/actions';
+import CountryDetails from '../presentational/CountryDetails';
+
+class CountryDetailsContainer extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    this.props.dispatch(getCountry(this.props.params.id))
+  }
+  render() {
+    return (
+      <div>
+        <CountryDetails country={this.props.selectedCountry} />
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = store => {
+  return {
+      selectedCountry: store.countriesReducer.selectedCountry
+  };
+};
+
+export default connect(mapStateToProps)(CountryDetailsContainer);
--- src/presentational/CountryFlag.js
+++ src/presentational/CountryFlag.js
@@ -4,7 +4,7 @@ import '../country.css';
 const countryFlag = (props) => {
   return (
     <div className="country-logo-wrapper">
-      <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
+      <img className="country-logo" src={props.country.imageUrl} alt="country" />
     </div>
   );
 };
