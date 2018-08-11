d1742784b222a4b1befe9930e326cfcd0d18f1a9
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-11 15:03:33 +0200

    Add button and it's logic for delete country

--- src/containers/CountryFlagContainer.js
+++ src/containers/CountryFlagContainer.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/CountryFlagList';
-import { getCountries, searchCountries } from '../actions/actions';
+import { getCountries, searchCountries, deleteCountry } from '../actions/actions';
 
 class CountryFlagContainer extends Component {
   constructor(props) {
     super(props);
   }
-  search(event) {
-    console.log(this.props)
-    this.props.dispatch(searchCountries(event.target.value));
-  }
+
   componentDidMount(props){
     this.props.dispatch(getCountries());
     this.props.dispatch(searchCountries(''));
   }
+
+  search(event) {
+    console.log(this.props)
+    this.props.dispatch(searchCountries(event.target.value));
+  }
+
+  deleteCountry(id){
+    this.props.dispatch(deleteCountry(id));
+  }
+
   render() {
     return (
       <div>
         <div className="search text-center">
           <input type="text" onChange={ this.search.bind(this) } />
         </div>
-        <CountryFlagList countries={this.props.visibleCountries} />
+        <CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)} />
       </div>
     );
   }
